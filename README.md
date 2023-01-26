2b26756ca4a162cac35d4e548715e9f42ef774f8
Sheogorath9 <ogarrett189@gmail.com>
2023-01-26 16:27:05 -0500

    updated README, deleted some notes from js  and definied some js variables

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # Work-Day-Scheduler-Final
 
-I will resubmit this project again once I have completed the local storage and the color coded time blocks that correspond to past/present/future. Thank you! 
+Thank you for taking the time to review my Work Day Scheduler app. In this app you will be able to keep track of your tasks during office hours; 9am-5pm. 
+
+You will be able to:
+
+See a time stamp of the date and time when you opened the app
+
+See a color coded work day scheduler for the hours of 9am-5pm
+
+Dynamic styling that indicates the time of day. Red for present hour; green for future; grey for the past. 
+
+You will be able to write tasks in each hour's text area and save your activity so that when you refresh the page, the activity will still appear. 
+
+Thanks for checking out my app! 
+
+
+https://sheogorath9.github.io/Work-Day-Scheduler-Final/
+
+<img src="/assets/deployed_application.png" alt="Front page of app" title="Deployed App">
--- script.js
+++ script.js
@@ -5,83 +5,26 @@ var timeDisplay = $("#currentDay");
 var saveBtn = $(".save-Btn");
 var textData = $("#textInfo");
 var texMes = [];
-
+// time block 
+var nineAm = $("hour-09");
+var tenAm = $("hour-10");
+var elevenAm = $("hour-11");
+var twelvePm = $("hour-12");
+var onePm = $("hour-13");
+var twoPm = $("hour-14");
+var threePm = $("hour-15");
+var fourPm = $("hour-16");
+var fivePm = $("hour-17");
 // display current time and date
-
 const currentDay = dayjs().format("dddd, MMMM D, hh:mm:ss a");
-/* const currentDayTextEl = document.getElementById('currentDay');
 
-currentDayTextEl.innerText = currentDay; */
-// $("#currentDay").text(currentDay);
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-// document.addEventListener("click", saveBtn);
-// console.log(saveBtn);
-
-
-/* 
-
-function display(){
-  localStorage.setItem("userText", input.value)
-  textInfo.innerHTML = localStorage.getItem("value")
-}
-*/
-
-
- $(function () {
+$(function () {
 
 $("#currentDay").text(currentDay);
 
 });
 
-var response = localStorage.getItem("myResponse");
-
-saveBtn.addEventListener("click", function(){
-  localStorage.setItem("myResponse", response)
-})
-
-/*
-
-// TODO: Add a listener for click events on the save button. This code should
-// use the id in the containing time-block as a key to save the user input in
-// local storage. HINT: What does `this` reference in the click listener
-// function? How can DOM traversal be used to get the "hour-x" id of the
-// time-block containing the button that was clicked? How might the id be
-// useful when saving the description in local storage?
-
-document.addEventListener("click", saveText);
-console.log(this.saveText);
-//
-// TODO: Add code to apply the past, present, or future class to each time
-// block by comparing the id to the current hour. HINTS: How can the id
-// attribute of each time-block be used to conditionally add or remove the
-// past, present, and future classes? How can Day.js be used to get the
-// current hour in 24-hour time?
-
-if (time === past) {
-  display = grey;
-} else if (time === future) {
-  display = green;
-} else {
-  display = red;
-}
-
-// TODO: Add code to get any user input that was saved in localStorage and set
-// the values of the corresponding textarea elements. HINT: How can the id
-// attribute of each time-block be used to do this?
-
-$("#hour-09 .description").val(localStorage.getItem("hour-09"));
-$("#hour-10 .description").val(localStorage.getItem("hour-10"));
-$("#hour-11 .description").val(localStorage.getItem("hour-11"));
-$("#hour-12 .description").val(localStorage.getItem("hour-12"));
-$("#hour-01 .description").val(localStorage.getItem("hour-01"));
-$("#hour-02 .description").val(localStorage.getItem("hour-02"));
-$("#hour-03 .description").val(localStorage.getItem("hour-03"));
-$("#hour-04 .description").val(localStorage.getItem("hour-04"));
-$("#hour-05 .description").val(localStorage.getItem("hour-05"));
-
- });
-
- */
+// Set Item
+localStorage.setItem("lastname", "Smith");
+// Retrieve
+document.getElementById("demo").innerHTML = localStorage.getItem("lastname");
